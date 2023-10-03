53455e3fc2147e8ad97e1a0c8aed5bdfb01fc878
Elliot Akande <elbo.lynch@gmail.com>
2023-10-03 12:23:51 +0100

    Create Ship hit method

--- src/Ship.js
+++ src/Ship.js
@@ -1,5 +1,11 @@
 const Ship = (length) => {
   let hits = 0;
+  return {
+    hit() {
+      if (hits < length) hits += 1;
+      return hits;
+    },
+  };
 };
 
 export default Ship;
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -1,1 +1,16 @@
 import Ship from "./Ship";
+
+describe(".hit()", () => {
+  test("Increments and returns hits", () => {
+    const ship = Ship(3);
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
+    expect(ship.hit()).toBe(3);
+  });
+  test("Stops incrementing at max", () => {
+    const ship = Ship(2);
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
+    expect(ship.hit()).toBe(2);
+  });
+});
