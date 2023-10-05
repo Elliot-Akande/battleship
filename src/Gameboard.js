58e8cce9a72b12de0aed4e926f9056c20b65e825
Elliot Akande <elbo.lynch@gmail.com>
2023-10-05 11:34:39 +0100

    Create placeShip method

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,39 @@
-const Gameboard = () => {};
+import Ship from "./Ship";
+
+const Cell = () => ({
+  isHit: false,
+});
+
+const Gameboard = () => {
+  const board = Array(10)
+    .fill()
+    .map(() => Array(10).fill(Cell()));
+
+  const placeShip = (x, y, length, axis) => {
+    if (axis === "x") {
+      if (x + length - 1 > 9) throw new Error("Placement out of bounds");
+    } else if (axis === "y") {
+      if (y - length + 1 < 0) throw new Error("Placement out of bounds");
+    } else {
+      throw new Error("Invalid axis prop");
+    }
+
+    const ship = Ship(length);
+    for (let i = 0; i < length; i += 1) {
+      if (axis === "x") {
+        board[x + i][y] = { ...board[x + i][y], ship };
+      } else {
+        board[x][y - i] = { ...board[x][y - i], ship };
+      }
+    }
+  };
+
+  const getBoard = () => board;
+
+  return {
+    placeShip,
+    getBoard,
+  };
+};
 
 export default Gameboard;
--- src/Gameboard.test.js
+++ src/Gameboard.test.js
@@ -1,1 +1,75 @@
 import Gameboard from "./Gameboard";
+
+describe(".placeShip()", () => {
+  let board;
+
+  beforeEach(() => {
+    board = Gameboard();
+  });
+
+  describe("Vertical ship", () => {
+    test("Ship spans 3 cells", () => {
+      board.placeShip(5, 5, 3, "y");
+      expect(board.getBoard()[5][5]).toHaveProperty("ship");
+      expect(board.getBoard()[5][4]).toHaveProperty("ship");
+      expect(board.getBoard()[5][3]).toHaveProperty("ship");
+    });
+
+    test("Ship ship doesn't exceed length", () => {
+      board.placeShip(5, 5, 3, "y");
+      const { ship } = board.getBoard()[5][5];
+
+      expect(board.getBoard()[5][5].ship).toBe(ship);
+      expect(board.getBoard()[5][4].ship).toBe(ship);
+      expect(board.getBoard()[5][3].ship).toBe(ship);
+
+      expect(board.getBoard()[5][6].ship).not.toBe(ship);
+      expect(board.getBoard()[5][2].ship).not.toBe(ship);
+    });
+
+    test("Throws error if ship overflows grid", () => {
+      expect(() => board.placeShip(0, 0, 3, "y")).toThrow(
+        /Placement out of bounds/
+      );
+      expect(() => board.placeShip(0, 1, 3, "y")).toThrow(
+        /Placement out of bounds/
+      );
+      expect(() => board.placeShip(0, 2, 3, "y")).not.toThrow(
+        /Placement out of bounds/
+      );
+    });
+  });
+
+  describe("Horizontal ship", () => {
+    test("Ship spans 3 cells", () => {
+      board.placeShip(5, 5, 3, "x");
+      expect(board.getBoard()[5][5]).toHaveProperty("ship");
+      expect(board.getBoard()[6][5]).toHaveProperty("ship");
+      expect(board.getBoard()[7][5]).toHaveProperty("ship");
+    });
+
+    test("Ship ship doesn't exceed length", () => {
+      board.placeShip(5, 5, 3, "x");
+      const { ship } = board.getBoard()[5][5];
+
+      expect(board.getBoard()[5][5].ship).toBe(ship);
+      expect(board.getBoard()[6][5].ship).toBe(ship);
+      expect(board.getBoard()[7][5].ship).toBe(ship);
+
+      expect(board.getBoard()[4][5].ship).not.toBe(ship);
+      expect(board.getBoard()[8][5].ship).not.toBe(ship);
+    });
+
+    test("Throws error if ship overflows grid", () => {
+      expect(() => board.placeShip(9, 9, 3, "x")).toThrow(
+        /Placement out of bounds/
+      );
+      expect(() => board.placeShip(8, 9, 3, "x")).toThrow(
+        /Placement out of bounds/
+      );
+      expect(() => board.placeShip(7, 9, 3, "x")).not.toThrow(
+        /Placement out of bounds/
+      );
+    });
+  });
+});
