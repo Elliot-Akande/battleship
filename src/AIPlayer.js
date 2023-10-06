ee3f4ece6c9a01b4608a8b4602cff6a4c953ea0d
Elliot Akande <elbo.lynch@gmail.com>
2023-10-06 12:25:49 +0100

    Create AIPlayer with placeShip method

---
+++ src/AIPlayer.js
@@ -0,0 +1,35 @@
+import Player from "./Player";
+
+const AIPlayer = () => {
+  const player = Player("CPU");
+
+  const placeShip = (length) => {
+    for (let i = 0; i < 100; i += 1) {
+      try {
+        const x = Math.floor(Math.random() * 10);
+        const y = Math.floor(Math.random() * 10);
+        const axis = Math.floor(Math.random() * 2) === 0 ? "x" : "y";
+        player.getBoard().placeShip(x, y, length, axis);
+        // console.table(
+        //   player
+        //     .getBoard()
+        //     .getBoard()
+        //     .map((row) =>
+        //       row.map((cell) =>
+        //         Object.prototype.hasOwnProperty.call(cell, "ship") ? "X" : ""
+        //       )
+        //     )
+        // );
+        return;
+      } catch (error) {}
+    }
+
+    throw new Error("Unable to generate ship placement");
+  };
+
+  return {
+    placeShip,
+  };
+};
+
+export default AIPlayer;
