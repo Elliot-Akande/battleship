9eb646c761f163523ef8fc5d649d823a46729eae
Elliot Akande <elbo.lynch@gmail.com>
2023-10-09 12:34:37 +0100

    Remove logging from placeShip

--- src/AIPlayer.js
+++ src/AIPlayer.js
@@ -10,16 +10,6 @@ const AIPlayer = () => {
         const y = Math.floor(Math.random() * 10);
         const axis = Math.floor(Math.random() * 2) === 0 ? "x" : "y";
         player.placeShip(x, y, length, axis);
-        // console.table(
-        //   player
-        //     .getBoard()
-        //     .getBoard()
-        //     .map((row) =>
-        //       row.map((cell) =>
-        //         Object.prototype.hasOwnProperty.call(cell, "ship") ? "X" : ""
-        //       )
-        //     )
-        // );
         return;
       } catch (error) {}
     }
