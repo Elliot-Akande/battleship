b1b9f59fe1209474f5a6cc9509733a67ef31e986
Elliot Akande <elbo.lynch@gmail.com>
2023-10-09 12:37:06 +0100

    Create setup method

--- src/GameController.js
+++ src/GameController.js
@@ -1,3 +1,4 @@
+import PubSub from "pubsub-js";
 import AIPlayer from "./AIPlayer";
 import Player from "./Player";
 
@@ -5,6 +6,30 @@ const GameController = () => {
   const ships = [2, 2, 2, 2, 3, 3, 3, 4, 4, 5];
   const playerOne = Player("Elliot");
   const playerTwo = AIPlayer();
+
+  playerOne.setOpponent(playerTwo);
+  playerTwo.setOpponent(playerOne);
+
+  const setup = () => {
+    ships.forEach((ship) => playerTwo.placeShip(ship));
+    ships.forEach((ship) => {
+      for (let i = 0; i < 100; i += 1) {
+        try {
+          const x = Math.floor(Math.random() * 10);
+          const y = Math.floor(Math.random() * 10);
+          const axis = Math.floor(Math.random() * 2) === 0 ? "x" : "y";
+          playerOne.placeShip(x, y, ship, axis);
+          return;
+        } catch (error) {}
+      }
+    });
+    PubSub.publish("SETUP COMPLETE");
+  };
+
+
+  return {
+    setup,
+  };
 };
 
 export default GameController;
