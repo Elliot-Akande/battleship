054ddbe8a073928ff1eb34f16f5dc8b10f8cbad4
Elliot Akande <elbo.lynch@gmail.com>
2023-10-16 10:50:56 +0100

    Create getGrid method

--- src/DisplayController.js
+++ src/DisplayController.js
@@ -1,3 +1,15 @@
-const DisplayController = () => {};
+const DisplayController = () => {
+  const getCell = (x, y) => {
+    const cell = document.createElement("div");
+    cell.dataset.x = x;
+    cell.dataset.y = y;
+    cell.classList.add("cell");
+    return cell;
+  };
+
+  return {
+    getCell,
+  };
+};
 
 export default DisplayController;
