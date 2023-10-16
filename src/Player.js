2c9f81c3833d15ef63c603836e236e03b2cb6bf6
Elliot Akande <elbo.lynch@gmail.com>
2023-10-16 11:57:15 +0100

    Implement placeShip with pub/sub pattern

--- src/Player.js
+++ src/Player.js
@@ -1,3 +1,4 @@
+import PubSub from "pubsub-js";
 import Gameboard from "./Gameboard";
 
 const Player = (name = "Player") => {
@@ -12,11 +13,17 @@ const Player = (name = "Player") => {
 
   const attack = (x, y) => opponent.receiveAttack(x, y);
 
+  const placeShip = (x, y, length, axis) => {
+    board.placeShip(x, y, length, axis);
+    PubSub.publish("SHIP_PLACED", { player: name, x, y, length, axis });
+  };
+
   return {
     ...board,
     getName,
     setOpponent,
     attack,
+    placeShip,
   };
 };
 
