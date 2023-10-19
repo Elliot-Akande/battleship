f4c47536b4bbacae3e0d15f92d91e998e15d7130
Elliot Akande <elbo.lynch@gmail.com>
2023-10-19 15:02:51 +0100

    Create core connect display and game controllers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+import PubSub from "pubsub-js";
+import GameController from "./GameController";
+import DisplayController from "./DisplayController";
+import Player from "./Player";
+import AIPlayer from "./AIPlayer";
+import "./style.css";
+
+const ships = [2, 3, 3, 4, 5];
+const playerOne = Player("playerOne");
+const playerTwo = AIPlayer();
+
+const game = GameController(playerOne, playerTwo, ships);
+const display = DisplayController();
+
+display.boardSetup(playerOne.getName(), ships);
+
+PubSub.subscribe("ALL_SHIPS_PLACED", () => {
+  game.setup();
+  display.newGrid(playerTwo.getName());
+});
